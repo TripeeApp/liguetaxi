c38dd464dfc4bdc26717d1abbab955f93cdee5d6
rafaeldias <rafaeldias.c@gmail.com>
2019-08-13 17:40:52 -0300

    fixed test case for default transport

--- client_test.go
+++ client_test.go
@@ -8,9 +8,9 @@ import (
 
 func TestNew(t *testing.T) {
 	testCases := []struct{
-		host string
-		token string
-		client *http.Client
+		host	string
+		token	string
+		client	*http.Client
 	}{
 		{"/", "abc", nil},
 		{"/", "abc", &http.Client{}},
@@ -28,16 +28,13 @@ func TestNew(t *testing.T) {
 			t.Errorf("got c.host : %s; want %s.", c.host, tc.host)
 		}
 
-		base := http.DefaultClient.Transport
+		tr := http.RoundTripper(&Transport{tc.token, http.DefaultClient.Transport})
 		if tc.client != nil {
-			base = tc.client.Transport
+			tr = tc.client.Transport
 		}
 
-		tr := &Transport{tc.token, base}
-
 		if !reflect.DeepEqual(c.client.Transport, tr) {
-			t.Errorf("got Transport %+v; want %+v.",
-				c.client.Transport, tr)
+			t.Errorf("got Transport %+v; want %+v.", c.client.Transport, tr)
 		}
 	}
 }
