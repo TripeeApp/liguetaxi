9e711d7dde5d973722cb80e99ecfcc25cec5cbc7
rafaeldias <rafaeldias.c@gmail.com>
2019-08-13 22:32:05 -0300

    Added implementation for encoding a body in Request method

--- client.go
+++ client.go
@@ -2,6 +2,9 @@ package liguetaxi
 
 import (
 	"context"
+	"encoding/json"
+	"bytes"
+	"io"
 	"net/http"
 	"net/url"
 )
@@ -31,6 +34,11 @@ func New(host *url.URL, token string, c *http.Client) *Client {
 // in which case it is resolved relative to the host of the Client.
 func (c *Client) Request(ctx context.Context, method, path string, body interface{}) (*http.Response, error) {
 	u, _ := c.host.Parse(path)
-	req, _ := http.NewRequest(method, u.String(), nil)
+	var b io.ReadWriter
+	if body != nil {
+		b = new(bytes.Buffer)
+		json.NewEncoder(b).Encode(body)
+	}
+	req, _ := http.NewRequest(method, u.String(), b)
 	return c.client.Do(req)
 }
