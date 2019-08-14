22f7a8d70ffae1da525c90406bcea7ca717abf7b
rafaeldias <rafaeldias.c@gmail.com>
2019-08-14 20:18:57 -0300

    Added implementation for empty context endpoint type

--- endpoint.go
+++ endpoint.go
@@ -5,12 +5,31 @@ import (
 	"fmt"
 )
 
-type ResponseTypeKey struct{}
+// ContextKey is just an empty struct. It exists so ResType
+// can be used as unique key for context.
+type contextKey struct{}
 
-type Endpoint string
+// ResType is the context key to use with context's
+// WithValue function to set the type of request's
+// payload that will be received from the API.
+var ResType contextKey
+
+// Request return types.
+const (
+	Json	= `json`
+	Xml	= `xml`
+)
+
+type endpoint string
 
 // String returns the endpoint suffixed with the type
 // of the request: json or xml.
-func (e *Endpoint) String(ctx context.Context) string {
-	return fmt.Sprintf("%s/%s", *e, `json`)
+// The suffix is read from the context. If there's
+// no suffix set in context, defaults to `json`.
+func (e *endpoint) String(ctx context.Context) string {
+	suffix := Json
+	if t, ok := ctx.Value(ResType).(string); ok && t != "" {
+		suffix = t
+	}
+	return fmt.Sprintf("%s/%s", *e, suffix)
 }
