5958c8b83c92300dabeed6b94782d7bf17045d2d
rafaeldias <rafaeldias.c@gmail.com>
2019-08-14 18:31:21 -0300

    Refactored String method

--- endpoint.go
+++ endpoint.go
@@ -1,11 +1,16 @@
 package liguetaxi
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type ResponseTypeKey struct{}
 
 type Endpoint string
 
+// String returns the endpoint suffixed with the type
+// of the request: json or xml.
 func (e *Endpoint) String(ctx context.Context) string {
-	return "/test/json"
+	return fmt.Sprintf("%s/%s", *e, `json`)
 }
