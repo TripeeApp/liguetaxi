915444bce3cf5ba7542ca75fd629d6b3ec86051b
rafaeldias <rafaeldias.c@gmail.com>
2019-08-14 23:37:18 -0300

    Added tests for ContextType method

--- endpoint_test.go
+++ endpoint_test.go
@@ -5,6 +5,40 @@ import (
 	"context"
 )
 
+func TestEndpointContextType(t *testing.T) {
+	tetCases := []struct{
+		ctx	 context.Context
+		endpoint endpoint
+		want	 string
+	}{
+		{
+			context.Background(),
+			endpoint("/test"),
+			Json,
+		},
+		{
+			context.WithValue(context.Background(), ResType, Json),
+			endpoint("/test"),
+			Json,
+		},
+		{
+			context.WithValue(context.Background(), ResType, Xml),
+			endpoint("/test"),
+			Xml,
+		}
+	}
+
+
+	for _, tc := range testCases {
+		e := tc.endpoint.ContextType(tc.ctx)
+
+		if e != tc.want {
+			t.Errorf("got Endpoint.ContextType(%+v): %s; want %s.", tc.ctx, e, tc.want)
+		}
+	}
+
+}
+
 func TestEndpointString(t *testing.T) {
 	testCases := []struct{
 		ctx	 context.Context
